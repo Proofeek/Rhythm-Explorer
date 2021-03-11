b4c049b9b34f4c3b9d886469721f54caf2e086ea
Proofeek <78957574+Proofeek@users.noreply.github.com>
2021-03-11 19:23:14 +0400

    MainMenu Improvements, Animation Optimization

--- Assets/Scripts/ButtonControllerMenu.cs
+++ Assets/Scripts/ButtonControllerMenu.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class ButtonControllerMenu : MonoBehaviour
 {
     public Animator ButtonAnimator;
+
+    public GameObject MainMenu;
+    public GameObject SettingsMenu;
+    public GameObject LevelMenu;
+
+    public int buttonIndex = 0; //0-MainMenu; 1-LevelMenu; 2-SettingsMenu
     void Start()
     {
         
@@ -17,11 +23,13 @@ public class ButtonControllerMenu : MonoBehaviour
     void OnTouchDown()
     {
         ButtonAnimator.SetTrigger("ButtonDown");
+        
     }
 
     void OnTouchUp()
     {
         ButtonAnimator.SetTrigger("ButtonUp");
+        StartCoroutine(ChangePreset(buttonIndex));
     }
     void OnTouchStay()
     {
@@ -30,5 +38,30 @@ public class ButtonControllerMenu : MonoBehaviour
     void OnTouchExit()
     {
         ButtonAnimator.SetTrigger("ButtonUp");
+    }
+
+    IEnumerator ChangePreset(int buttonIndex)
+	{
+        yield return new WaitForSeconds(0.8f);
+        if(buttonIndex == 0)
+		{
+            SettingsMenu.gameObject.SetActive(false);
+            LevelMenu.gameObject.SetActive(false);
+            MainMenu.gameObject.SetActive(true);
+        }
+        if(buttonIndex == 1)
+
+        {
+            SettingsMenu.gameObject.SetActive(false);
+            LevelMenu.gameObject.SetActive(true);
+            MainMenu.gameObject.SetActive(false);
+        }
+        if(buttonIndex == 2)
+
+        {
+            SettingsMenu.gameObject.SetActive(true);
+            LevelMenu.gameObject.SetActive(false);
+            MainMenu.gameObject.SetActive(false);
+        }
     }
 }
